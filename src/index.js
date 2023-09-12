24c4f242003ca55c92d146fd1227395d529d8d08
siddh-kivtechs <siddhbose@kneoginigmisarch.co.in>
2023-09-12 15:09:04 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,16 @@
-const fetch = require("node-fetch");
+// In src/index.js
 const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const ejs = require('ejs');
-const path = require("path");
-const { sql } = require("@vercel/postgres");
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.use(cors());
-const url = "https://www.google.com";
+
+// *** REMOVE ***
 app.get("/", (req, res) => {
-  fetch(url)
-  .then(response => response.text())
-  .then(text => console.log(text));
-  res.send(text);
+  res.send("<h2>It's Working!</h2>");
 });
 
 
 app.listen(PORT, () => {
   console.log(`API is listening on port ${PORT}`);
 });
-
-
