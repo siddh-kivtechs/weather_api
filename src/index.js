112a157a801788f71a25238f529fef236744f749
siddh-kivtechs <siddhbose@kneoginigmisarch.co.in>
2023-09-12 15:17:46 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ const fetch = require("node-fetch");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-function get_data(url)
-{
+// function get_data(url)
+// {
   
-fetch(url)
-  .then(response => response.text())
-  return text;
-}
+// fetch(url)
+//   .then(response => response.text())
+//   return text;
+// }
 app.get("/", (req, res) => {
   res.send("<h2>It's Working!</h2>");
 });
