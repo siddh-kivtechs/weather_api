8adca5e42e40821d640c762d38b5cfc16143d2de
siddh-kivtechs <siddhbose@kneoginigmisarch.co.in>
2023-09-13 00:35:08 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ import { get_data } from 'get_data';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-function get_data(url)
-{
+// function get_data(url)
+// {
   
-fetch(url)
-  .then(response => response.text())
-  return text;
-}
+// fetch(url)
+//   .then(response => response.text())
+//   return text;
+// }
 app.get("/", (req, res) => {
   const url = "https://siddh-kivtechs.github.io/menu_kivtechs_json/part_10.json";
   res.send(get_data(url));
